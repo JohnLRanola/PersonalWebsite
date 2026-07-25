21235c8be014140e7bd9e773edfbf85faf23bd2b
JohnLRanola <johnlfranola@gmail.com>
2026-07-25 21:23:44 +0100

    -Added Template for Site Domain and Trailing Slash for later on

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    // Site URL for sitemap generation, RSS feeds, etc.
+    site: "https://www.example.com", // Replace Later On
+    trailingSlash: "always",
+});
